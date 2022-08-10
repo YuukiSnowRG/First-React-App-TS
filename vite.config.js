a2e152b0754aaa986b33ab2c01370fe34417f6d2
Yuki Snow <yuukisnowg@gmail.com>
2022-08-10 00:31:44 -0300

    Adding dist2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/viteappsample/",
+  base:"/First-React-App-TS/",
   plugins: [react()]
 })
